753417ed44aae467d9061f431f9fb86f9880152c
Jext <jextious@gmail.com>
2018-03-09 19:58:05 +0100

    Minor cleanup

--- index.js
+++ index.js
@@ -8,18 +8,10 @@ const Viridine = require("./lib/viridine"),
 		process.kill(process.pid, signal);
 	};
 
-process.on("SIGINT", () => {
-	exitHandler("SIGINT");
-});
+process.on("SIGINT", () => exitHandler("SIGINT"));
 
-process.on("SIGHUP", () => {
-	exitHandler("SIGHUP");
-});
+process.on("SIGHUP", () => exitHandler("SIGHUP"));
 
-process.on("SIGTERM", () => {
-	exitHandler("SIGTERM");
-});
+process.on("SIGTERM", () => exitHandler("SIGTERM"));
 
-process.on("SIGQUIT", () => {
-	exitHandler("SIGQUIT");
-});
+process.on("SIGQUIT", () => exitHandler("SIGQUIT"));
